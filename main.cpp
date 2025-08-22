e044e7b690776ad648e582b424a36203efcf3f03
jmznnva <jmznnva88@gmail.com>
2025-08-22 12:10:59 -0400

    Added a new line for the NewBrach

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello, this is my repo";
+	std::cout << "Hello, this is my repo\n";
+	std::cout << "and now I've added a branch\n";
 	return 0;
 }
